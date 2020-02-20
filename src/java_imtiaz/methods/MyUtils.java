0a0b37366e84b7df747f4e2a022c9acd47e4f7a2
parvej <parvej1114@yahoo.com>
2020-02-20 16:27:46 +0800

    added 2 method class added

---
+++ src/java_imtiaz/methods/MyUtils.java
@@ -0,0 +1,18 @@
+package java_imtiaz.methods;
+
+public class MyUtils {
+
+    public static void printSomeJunk(String argument){
+        System.out.println("some bla bla bla. " + argument);
+    }
+    public static void printSomeJunk(int argument){
+        System.out.println("i am " + argument + " years old.");
+    }
+
+    public static void sum2Numbers(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+    public static int add10(int someArgument){
+        return someArgument+10;
+    }
+}
---
+++ src/java_imtiaz/methods/learningMethods.java
@@ -0,0 +1,13 @@
+package java_imtiaz.methods;
+
+public class learningMethods {
+    public static void main(String[] args) {
+        System.out.println("lkfdslkf");
+        MyUtils.printSomeJunk("hi this is pax");
+        MyUtils.printSomeJunk(25);
+        MyUtils.sum2Numbers(10,5);
+        //int myVar = MyUtils.add10(80);
+        System.out.println(MyUtils.add10(80));
+    }
+
+}
