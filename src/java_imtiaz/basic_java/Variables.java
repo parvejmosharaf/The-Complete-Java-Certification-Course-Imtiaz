3f96f99ed79ef4f8d44e6e762bfe1705e724dee5
parvej <parvej1114@yahoo.com>
2020-02-20 14:58:40 +0800

    added Variables class.

---
+++ src/java_imtiaz/basic_java/Variables.java
@@ -0,0 +1,17 @@
+package java_imtiaz.basic_java;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 34;
+        System.out.println(x);
+        x = 550;
+
+        System.out.println(x + 4);
+
+        System.out.println(x);
+        String words = "This is a sentence.";
+        System.out.println(words + " there have some more words.");
+
+
+    }
+}
