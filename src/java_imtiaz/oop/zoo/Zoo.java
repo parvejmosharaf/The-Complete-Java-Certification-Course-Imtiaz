fe6eb86b8bda3085ed9ed6e5f87fd5d938a1aff8
parvej <parvej1114@yahoo.com>
2020-02-21 22:17:15 +0800

    test the interface

--- src/java_imtiaz/oop/zoo/Zoo.java
+++ src/java_imtiaz/oop/zoo/Zoo.java
@@ -3,18 +3,9 @@ package java_imtiaz.oop.zoo;
 
 public class Zoo {
     public static void main(String[] args) {
-        Animal sparrow1 = new Sparrow("pag", 1,1.4);
-        sparrow1.move();
-        //sparrow1.fly();
+        Flyable flyableBird = new Sparrow("magpi",3,1.3);
+        flyableBird.fly();
 
-        Animal fish1 = new Fish("carp", 1, 1.3);
-        fish1.move();
-        //fish1.swim();
-        moveAnimals(sparrow1);
-        moveAnimals(fish1);
-
-    }
-    public static void moveAnimals(Animal animal){
-        animal.move();
+      //  Flyable flyableBird1 = new Bird("mag",2,1.3);
     }
 }
